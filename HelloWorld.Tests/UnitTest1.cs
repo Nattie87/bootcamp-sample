01bd61fd3213b75179d3982eec82edda4b1b1708
Petra Barbieri <petra.barbieri@asos.com>
2017-09-18 15:52:37 +0100

    p says heyyy

--- HelloWorld.Tests/UnitTest1.cs
+++ HelloWorld.Tests/UnitTest1.cs
@@ -1,14 +1,4 @@
 using System;
 using Xunit;
 
-namespace HelloWorld.Tests
-{
-    public class UnitTest1
-    {
-        [Fact]
-        public void Test1()
-        {
-            Assert.True(true);
-        }
-    }
 }
